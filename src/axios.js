be975ec5fb2dacc3c5e06ab5f89e0f8f573f3308
3cor <spacecorgi@pm.me>
2020-11-13 18:46:29 +0700

    Change env variable name

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import store from '../src/store/store'
 
-const baseURL = process.env.BASE_URL
+const baseURL = process.env.VUE_APP_BASE_URL
 
 const instance = axios.create({ baseURL })
 instance.interceptors.response.use(

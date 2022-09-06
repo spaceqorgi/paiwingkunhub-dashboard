5d35233ea84f91af09cc065716663f4b672ae2dd
nohello <spacecorgi@pm.me>
2022-09-06 20:00:17 +0700

    Update

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import store from '../src/store/store'
 
-const baseURL = process.env.VUE_APP_BASE_URL
+const baseURL = process.env.VUE_APP_BASE_URL || 'https://pwg-api.proen.app.ruk-com.cloud'
 
 const instance = axios.create({ baseURL })
 instance.interceptors.response.use(
